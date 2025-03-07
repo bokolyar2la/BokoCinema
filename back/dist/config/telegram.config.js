5a2eaf38c02b7db88d8d21d667c4723958e92581
Daniil Bokoliar <143665403+bokolyar2la@users.noreply.github.com>
2025-03-07 08:03:53 -0800

    Removed dist folder from Git tracking

--- back/dist/config/telegram.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTelegramConfig = void 0;
-const getTelegramConfig = () => ({
-    chatId: '726959884',
-    token: '7304485880:AAHTNQK2Zzw2gNUbA7xaaBXvLBVZz3VW0Tw',
-});
-exports.getTelegramConfig = getTelegramConfig;
-//# sourceMappingURL=telegram.config.js.map
